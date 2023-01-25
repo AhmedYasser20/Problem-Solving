838f7f569cbc822e5efe7b6d050faf9e30c860fd
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2023-01-25 21:56:46 +0200

    Add files via upload

---
+++ CodeForces C/1511C.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include<set>
+#include<list>
+#include<map>
+#include<utility>
+//#define char cs[] = "abcdefghijklmnopqrstuvwxyz";
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define revsort(a,b) sort(a,b),reverse(a,b)
+typedef long long ll;
+int dx[9] = { 0,1,-1,1,-1,0,1,-1,0 };
+int dy[9] = { 0,0,0,1,-1,-1 ,-1,1,1 };
+using namespace std;
+const int MAX = 1000;
+int grid[MAX][MAX] = { 0 };
+//void bigint(string s1, string s2) {
+//	reverse(s1.begin(), s1.end());
+//	reverse(s2.begin(), s2.end());
+//	int a[MAX];
+//	memset(a, 0, sizeof(a));
+//	for (int i = 0; i < s1.length(); i++) {
+//		for (int j = 0; j < s2.length(); j++) {
+//			a[i + j] += (s1[i] - '0') + (s2[j] - '0');
+//		}
+//	}
+//	for (int i = 0; i < MAX - 1; i++) {
+//		a[i + 1] += a[i] / 10;
+//		a[i] %= 10;
+//	}
+//}
+int stringTointeger(string str)
+{
+    int temp = 0;
+    for (int i = 0; i < str.length(); i++) {
+        temp = temp * 10 + (str[i] - '0');
+    }
+    return temp;
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t = 1;
+	//cin >> t;
+	
+
+	while (t--) {
+		int q,n;
+		cin>>n>>q;
+		stack<int> MainStack;
+		vector<int> x;
+		lp(i,n){
+			int y;
+			cin>>y;
+			x.push_back(y);
+		}
+		for(int i=n-1;i>=0;i--){
+			MainStack.push(x[i]);
+		}
+		lp(i,q){
+			stack<int> Temp;
+			int y,pos=1;
+
+			cin>>y;
+			while(y!=MainStack.top()){
+				Temp.push(MainStack.top());
+				MainStack.pop();
+				pos++;
+			}
+			MainStack.pop();
+			cout<<pos<<" ";
+			while(!Temp.empty()){
+				MainStack.push(Temp.top());
+				Temp.pop();
+			}
+			MainStack.push(y);
+
+		}
+		
+		
+		
+		
+		
+	
+
+		
+	}
+}
+
+
+
+
