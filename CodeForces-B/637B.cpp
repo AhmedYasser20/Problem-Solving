fc0ec9c6d990303c14f1919d190e5980e9f9f35d
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2023-02-07 01:59:06 +0200

    Create 637B.cpp

---
+++ CodeForces-B/637B.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include<set>
+#include<list>
+#include<map>
+#include<utility>
+#include <cstring>
+//#define char cs[] = "abcdefghijklmnopqrstuvwxyz";
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define revsort(a,b) sort(a,b),reverse(a,b)
+typedef long long ll;
+int dx[9] = { 0,1,-1,1,-1,0,1,-1,0 };
+int dy[9] = { 0,0,0,1,-1,-1 ,-1,1,1 };
+using namespace std;
+const int MAX = 1000;
+int grid[MAX][MAX] = { 0 };
+//void bigint(string s1, string s2) {
+//	reverse(s1.begin(), s1.end());
+//	reverse(s2.begin(), s2.end());
+//	int a[MAX];
+//	memset(a, 0, sizeof(a));
+//	for (int i = 0; i < s1.length(); i++) {
+//		for (int j = 0; j < s2.length(); j++) {
+//			a[i + j] += (s1[i] - '0') + (s2[j] - '0');
+//		}
+//	}
+//	for (int i = 0; i < MAX - 1; i++) {
+//		a[i + 1] += a[i] / 10;
+//		a[i] %= 10;
+//	}
+//}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   //freopen("in.text", "r", stdin);
+  // freopen("out.text", "w", stdout);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+      int n;
+      cin>>n;
+      stack<string> st;
+      lp(i,n){
+        string x;
+        cin>>x;
+        st.push(x);
+      }
+      map<string ,int> mp;
+      while (!st.empty())
+      {
+        string temp=st.top();
+        if(mp[temp]==0){
+            cout<<temp<<endl;
+            mp[temp]++;
+        }
+        st.pop();
+      }
+      
+
+    }
+}
+
